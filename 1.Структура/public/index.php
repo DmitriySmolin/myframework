c5674d3744e78010d3e87eb35c2d186ecb513593
Dmitry <neox56@gmail.com>
2022-12-02 06:48:42 +0400

    Add  lesson 1

---
+++ 1.Структура/public/index.php
@@ -0,0 +1,20 @@
+<?php
+
+$query = rtrim($_SERVER['QUERY_STRING'],'/');
+
+require '../vendor/core/Router.php';
+require '../vendor/libs/functions.php';
+
+echo $query;
+
+Router::add('posts/add', ['controller' => 'Posts', 'action' => 'add']);
+Router::add('posts', ['controller' => 'Posts', 'action' => 'index']);
+Router::add('', ['controller' => 'Posts', 'action' => 'index']);
+
+debug(Router::getRoutes());
+
+if (Router::matchRoute($query)) {
+    debug(Router::getRoute());
+} else {
+    echo '404';
+}
